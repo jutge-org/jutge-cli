7a2ce8b80c8f9f4a6fcf8b34e854397d83c59e30
Pau Fernández <pau.fernandez@upc.edu>
2024-10-03 14:30:56 +0200

    Improved "tables" command
    - Refactored to reduce repetition.
    - Introduced the "-f" (format) option.
    - Implemented maximum width in print.table.

--- src/print.ts
+++ src/print.ts
@@ -1,32 +1,67 @@
-import chalk from 'chalk'
-import Table from 'cli-table3'
+import chalk from "chalk"
+import Table from "cli-table3"
+import yaml from 'yaml'
 
 export default {
-    normal: (x: any) => {
+    normal(x: any) {
         console.log(JSON.stringify(x, null, 2))
     },
 
-    error: (x: any) => {
+    error(x: any) {
         console.log(chalk.red(x))
     },
 
-    warning: (x: any) => {
+    warning(x: any) {
         console.log(chalk.yellow(x))
     },
 
-    success: (x: any) => {
+    success(x: any) {
         console.log(chalk.green(x))
     },
 
-    primary: (x: any) => {
+    primary(x: any) {
         console.log(chalk.blue(x))
     },
 
-    verticalTable: (data: any) => {
-        var table = new Table()
+    verticalTable(data: any) {
+        let table = new Table()
         for (const key in data) {
             table.push({ [key]: data[key] })
         }
         console.log(table.toString())
+    },
+
+    json(data: any) {
+        console.log(JSON.stringify(data, null, 2))
+    },
+
+	yaml(data: any) {
+		console.log(yaml.stringify(data))
+	},
+
+    table(data: Record<string, Record<string, any>>) {
+        const MAX_COL_WIDTH = 70
+        const numItems = Object.keys(data).length
+        if (numItems > 0) {
+            const entries = Object.entries(data)
+            const head = Object.keys(entries[0][1])
+
+            // Compute the maximum widths and truncate them if they are too long
+            let maxWidths = head.map((h) => h.length + 2)
+            for (const [_, value] of entries) {
+                const lengths = Object.values(value).map((x) => String(x).length + 2)
+                maxWidths = maxWidths.map((mx, j) => Math.max(mx, lengths[j]))
+            }
+            const colWidths = maxWidths.map((x) => Math.min(x, MAX_COL_WIDTH))
+
+            // Create the table and fill it
+            const table = new Table({ head, wordWrap: true, colWidths })
+            for (const [_, value] of entries) {
+                table.push(Object.values(value))
+            }
+
+            // Print it
+            console.log(table.toString())
+        }
     },
 }
--- src/tables.ts
+++ src/tables.ts
@@ -7,44 +7,25 @@ const jom = new JutgeObjectModel()
 
 export const tablesCmd = new Command("tables").description("Get tables")
 
-tablesCmd
-    .command("languages")
-    .description(`Display languages table`)
-    .action(async () => {
-        print.normal(await jom.languages.all())
-    })
+const formattedPrintFunctions = {
+	json: print.json,
+	yaml: print.yaml,
+	table: print.table,
+	yml: print.yaml,
+}
 
-tablesCmd
-    .command("countries")
-    .description(`Display countries table`)
-    .action(async () => {
-        print.normal(await jom.countries.all())
-    })
 
-tablesCmd
-    .command("compilers")
-    .description(`Display compilers table`)
-    .action(async () => {
-        print.normal(await jom.compilers.all())
-    })
-
-tablesCmd
-    .command("drivers")
-    .description(`Display drivers table`)
-    .action(async () => {
-        print.normal(await jom.drivers.all())
-    })
-
-tablesCmd
-    .command("verdicts")
-    .description(`Display verdicts table`)
-    .action(async () => {
-        print.normal(await jom.verdicts.all())
-    })
-
-tablesCmd
-    .command("proglangs")
-    .description(`Display proglangs table`)
-    .action(async () => {
-        print.normal(await jom.proglangs.all())
-    })
+for (const table of ["languages", "countries", "compilers", "drivers", "verdicts", "proglangs"]) {
+    tablesCmd
+        .command(table)
+        .description(`Show ${table} table`)
+        .option("-f <format>", "Choose format: json, yaml, table", "table")
+        .action(async (options) => {
+            const func = formattedPrintFunctions[options.f]
+            if (func) {
+                func(await jom[table].all())
+            } else {
+                print.error(`Unknown format: ${options.f}\nValid formats are: ${Object.keys(formattedPrintFunctions).join(", ")}`)
+            }
+        })
+}
