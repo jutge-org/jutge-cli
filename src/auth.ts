48bf409a3ef391b1563916ef6e15c0ce6e002eaa
Pau Fernández <pau.fernandez@upc.edu>
2024-10-04 11:16:01 +0200

    Integrated jom in the auth module

--- src/auth.ts
+++ src/auth.ts
@@ -1,64 +1,53 @@
-import { config } from './config'
-import { Command } from '@commander-js/extra-typings'
-import { password as pswd } from '@inquirer/prompts'
-import dayjs from 'dayjs'
-import relativeTime from 'dayjs/plugin/relativeTime'
-import { ensure_credentials, ensure_initialized } from './base'
-import { AuthService } from './client'
-import print from './print'
+import { Command } from "@commander-js/extra-typings"
+import { password as pswd } from "@inquirer/prompts"
+import dayjs from "dayjs"
+import relativeTime from "dayjs/plugin/relativeTime"
+import { ensure_credentials, ensure_initialized } from "./base"
+import { config } from "./config"
+import { jom } from "./jom"
+import print from "./print"
 
 dayjs.extend(relativeTime)
 
-export const authCmd = new Command('auth').description('Auhentication commands')
+export const authCmd = new Command("auth").description("Auhentication commands")
 
 authCmd
-    .command('login')
-    .description('Login to Jutge.org')
+    .command("login")
+    .description("Login to Jutge.org")
     .action(async () => {
         ensure_initialized()
-        const email = config.get('user.email') as string
+        const email = config.get("user.email") as string
         const password = await pswd({
             message: `Password for ${email} at Jutge.org?`,
             mask: true,
         })
-        try {
-            const credentials = await AuthService.login({
-                requestBody: { email, password },
-            })
-            config.set('credentials', credentials)
-            print.success('Login successful')
-        } catch (error) {
-            print.error('Login unsuccessful')
+        const { success, error } = await jom.auth.login(email, password)
+        if (success) {
+            print.success("Login successful")
+        } else {
+            print.error("Login unsuccessful")
+            print.error(error)
         }
     })
 
 authCmd
-    .command('logout')
-    .description('Logout from Jutge.org')
+    .command("logout")
+    .description("Logout from Jutge.org")
     .action(async () => {
         ensure_credentials()
-        try {
-            await AuthService.logout()
-            config.delete('credentials')
-            print.success('Logout successful')
-        } catch (error) {
-            print.error('Logout unsuccessful')
-        }
+        await jom.auth.logout()
+        print.success("Logout successful")
     })
 
 authCmd
-    .command('check')
-    .description('Check if logged in at Jutge.org')
+    .command("check")
+    .description("Check if logged in at Jutge.org")
     .action(async () => {
         ensure_credentials()
-        try {
-            const credentials = config.get('credentials', null) as any
-            if (!credentials) throw new Error('Not logged in')
-            const data = await AuthService.check()
-            if (!data.success) throw new Error('Not logged in')
-            const remaining = dayjs(credentials.expiration).from(dayjs())
-            print.success(`You are logged in (expiration ${remaining})`)
-        } catch (error) {
-            print.error('You are not logged in')
+        const { success, error, expiresIn } = await jom.auth.check()
+        if (success) {
+            print.success(`You are logged in (expiration in ${expiresIn})`)
+        } else {
+            print.error(error)
         }
     })
