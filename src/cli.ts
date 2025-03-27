a0c25dd017e7a7aa67c678e426449ca2a1fd7174
Pau Fernández <pau.fernandez@upc.edu>
2025-03-27 09:19:25 +0100

    Catch exceptions related to directory not loading and show a proper message

--- src/cli.ts
+++ src/cli.ts
@@ -1,19 +1,28 @@
 import { Command } from "@commander-js/extra-typings"
+import chalk from "chalk"
 import { accountCmd } from "./auth/accounts"
 import { loginCmd, logoutCmd } from "./auth/auth"
 import { loadDirectory } from "./directory/load-directory"
+import { JUTGE_API_URL } from "./env"
 import { moduleCommand } from "./module-cmd"
 import { versionCmd } from "./version"
 
 export const createCli = async () => {
-    const directory = await loadDirectory()
-    const cli = new Command().name("jutge").description("Jutge.org CLI")
-    cli.addCommand(loginCmd)
-    cli.addCommand(logoutCmd)
-    for (const module of directory.root.submodules) {
-        cli.addCommand(moduleCommand(module, ""))
+    try {
+        const directory = await loadDirectory()
+        const cli = new Command()
+            .name("jutge")
+            .description(`Jutge.org CLI [${chalk.blue(JUTGE_API_URL)}]`)
+        cli.addCommand(loginCmd)
+        cli.addCommand(logoutCmd)
+        for (const module of directory.root.submodules) {
+            cli.addCommand(moduleCommand(module, ""))
+        }
+        cli.addCommand(accountCmd)
+        cli.addCommand(versionCmd)
+        return cli
+    } catch (e) {
+        console.error(`Could not load directory.\n(Jutge API should be at: ${JUTGE_API_URL})\n`)
+        process.exit(1)
     }
-    cli.addCommand(accountCmd)
-    cli.addCommand(versionCmd)
-    return cli
 }
